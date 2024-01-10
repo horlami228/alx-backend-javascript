6605229569670ed9e6a84e0580b8845195b5a6bd
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-10 11:51:10 +0100

    data

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guilluame',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
+
+module.exports = getListStudents;
