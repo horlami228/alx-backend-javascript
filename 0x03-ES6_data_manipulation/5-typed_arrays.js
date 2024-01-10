78971d30193ba70c17004d10e9d15199da5ad8d5
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-10 14:59:16 +0100

    typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
+
+module.exports = createInt8TypedArray;
