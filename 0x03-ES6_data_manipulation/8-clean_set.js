e05ce59570cfd7bb0c1ba212cfc2fc4515acf4da
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-11 00:23:09 +0100

    fixed 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,6 +3,14 @@ function cleanSet(set, startString) {
     return '';
   }
 
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
+  if (startString === undefined) {
+    return '';
+  }
+
   const setFiltered = Array.from(set).filter((element) => element.startsWith(startString))
     .map((element) => element.slice(startString.length)).join('-');
 
