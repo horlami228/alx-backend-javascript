0227260e581495d030e6087568c897df99f13aec
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-11 22:36:21 +0100

    typescript_work

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,42 @@ export interface Teacher {
   location: string;
   [key: string]: unknown;
 }
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+export interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+  
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working'
+  }
+
+  displayName(): string {
+    return `${this.firstName}`
+  }
+
+}
