dc0c8ae95abdb31de207bcd68742df9b35c221ce
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-11 15:03:02 +0100

    typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,49 +1,8 @@
-interface Student {
-  firstName: string;
-  lastname: string;
-  age: number;
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
   location: string;
+  [key: string]: unknown;
 }
-
-const student1: Student = {
-  firstName: 'John',
-  lastname: 'Doe',
-  age: 25,
-  location: 'New York'
-}
-
-const student2: Student = {
-  firstName: 'Simion',
-  lastname: 'James',
-  age: 20,
-  location: 'San Fransisco'
-
-}
-
-const studentsList: Student[] = [student1, student2]
-
-// Render table using Vanilla JavaScript
-const table = document.createElement('table');
-const headerRow = table.insertRow(0);
-
-// Add header cells
-const headerCell1 = headerRow.insertCell(0);
-headerCell1.textContent = 'First Name';
-
-const headerCell2 = headerRow.insertCell(1);
-headerCell2.textContent = 'Location';
-
-// Add rows for each student
-studentsList.forEach((student, index) => {
-  const row = table.insertRow(index + 1);
-
-  // Add cells with student information
-  const cell1 = row.insertCell(0);
-  cell1.textContent = student.firstName;
-
-  const cell2 = row.insertCell(1);
-  cell2.textContent = student.location;
-});
-
-// Append the table to the document body
-document.body.appendChild(table);
--- 0x04-TypeScript/task_1/js/test.ts
+++ 0x04-TypeScript/task_1/js/test.ts
@@ -1,1 +1,10 @@
-console.log('lekan')
+import {Teacher} from './main';
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+}
+console.log(teacher1)
