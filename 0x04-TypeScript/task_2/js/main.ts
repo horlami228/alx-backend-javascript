d56545740c500ccb6118982d3f90ec5acebc5b61
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-12 23:28:22 +0100

    updated

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -63,3 +63,13 @@ export function executeWork(employee: (Director | Teacher)) {
   }
 }
 
+export type Subjects = ("Math" | "History");
+
+export function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History'
+  }
+}
+
