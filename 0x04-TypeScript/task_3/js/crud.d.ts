1f7c75ad180c62ec847853102420198aa9486055
Olamilekan <akintolaolamilekan51@gmail.com>
2024-01-13 00:27:54 +0100

    typescript_3

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,9 @@
+import {RowID, RowElement} from './interface'
+
+declare function insertRow(row: RowElement);
+
+declare function deleteRow(rowId: RowID);
+
+declare function updateRow(rowId: RowID, row: RowElement);
+
+
