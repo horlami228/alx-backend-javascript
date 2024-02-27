c6ce44b33fc70717eddf324da658b8133d13826d
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-27 22:47:44 +0100

    Remove unnecessary comments

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -8,7 +8,6 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * 
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
